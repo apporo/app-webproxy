9b3ce77fae53cdb97b323bbd6948b7ca01fd0aed
pnhung177 <pnhung177@acegik.net>
2016-11-01 11:35:31 +0700

    Clean source code

--- config/sandbox.js
+++ config/sandbox.js
@@ -1,15 +1,4 @@
 module.exports = {
-  bridges: {
-    appWebrouterMongodbWrapper: {
-      mongodb: {
-        connection_options: {
-          host: 'localhost',
-          port: 3306,
-          name: 'app-webrouter'
-        }
-      }
-    }
-  },
   plugins: {
     appWebrouter: {
       contextPath: '/webrouter',
