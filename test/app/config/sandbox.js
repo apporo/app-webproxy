e460d020795becd26cacb67c5c05952ab943e1da
pnhung177 <pnhung177@acegik.net>
2018-04-28 14:13:44 +0700

    Improve configure

--- test/app/config/sandbox.js
+++ test/app/config/sandbox.js
@@ -20,6 +20,37 @@ module.exports = {
               port: 15672,
               url: '$1'
             }
+          },
+          {
+            "id": "elasticsearch-plugins",
+            "enabled": false,
+            "source": {
+              "url": "/tool(.*)",
+              "methods": ["GET", "PUT"]
+            },
+            "target": {
+              "hostname": "opflow-broker-default",
+              "port": 9200,
+              "url": "$1",
+              "authentication": {
+                "enabled": false,
+                "type": "basic",
+                "username": "nobody",
+                "password": "secret"
+              },
+              "request": {
+                "headers": [{
+                  "name": "accept-encoding",
+                  "value": "*;q=1,gzip=0"
+                }]
+              },
+              "response": {
+                "headers": [{
+                  "name": "content-security-policy",
+                  "value": null
+                }]
+              }
+            }
           }
         ]
       }
